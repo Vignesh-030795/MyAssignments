ac77fcd9fa71ec37128b8e82bc12e06a5934acaf
Vignesh-030795 <vigneshganapathy95@gmail.com>
2025-05-25 08:56:21 +0530

    Add files via upload
    Day1 24/05/2025

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version=91.0f;
+		String developer="Google";
+		Boolean isBeta=false;
+		int releaseyear=2008;
+		char shortcutkey='c';
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseyear);
+		System.out.println(shortcutkey);
+
+	}
+
+}
